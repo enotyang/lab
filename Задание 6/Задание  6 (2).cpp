7b857ba868a51f3abda16480e71fcc18a0cc8c87
enotyang <71381990+enotyang@users.noreply.github.com>
2020-09-27 10:47:19 +0300

    Add files via upload

---
+++ Задание 6/Задание  6 (2).cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+int main()
+{
+	int A, B, C, t, f;
+	scanf_s("%d %d %d", &A, &B, &C);
+	t = A;
+	f = C;
+	C = B;
+	A = f;
+	B = t;
+	printf("%d %d %d\n", A, B, C);
+	return(0);
+}
