4846e486419d0cd9ae6bf22d46cff671c5354123
enotyang <71381990+enotyang@users.noreply.github.com>
2021-01-09 23:41:39 +0500

    Add files via upload

---
+++ Задание 17/Задание 17 (1).cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int i, n, k, l, s = 0, arr[1000];
+    float c = 0, r;
+    printf("Введите K, L и кол-во элементов N: ");
+    scanf_s("%d %d %d", &k, &l, &n);
+    printf("Введите элементы массива: ");
+    for (i = 0; i < n; i++)
+        scanf_s("%d", &arr[i]);
+    for (i = k - 1; i <= l - 1; i++) {
+        s = s + arr[i];
+        c = c + 1;
+    }
+    r = s / c;
+    printf("%f", r);
+    return 0;
+}
