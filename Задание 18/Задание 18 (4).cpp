7a27298bab553650bd16e23ecaccdffdd173284e
enotyang <71381990+enotyang@users.noreply.github.com>
2021-01-12 17:53:28 +0500

    Add files via upload

---
+++ Задание 18/Задание 18 (4).cpp
@@ -0,0 +1,24 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int i, n, max = 0, min = 0, a[1000];
+    printf("Введите кол-во элементов N: ");
+    scanf_s("%d", &n);
+    printf("Введите элементы массива A: ");
+    for (i = 0; i < n; i++)
+        scanf_s("%d", &a[i]);
+    for (i = 0; i < n; i++) {
+        if (a[i] > a[max]) max = i;
+        if (a[i] < a[min]) min = i;
+    }
+    if (min > max) 
+        for (i = max + 1; i < min; i++)
+            a[i] = 0;
+    else
+        for (i = min + 1; i < max; i++)
+            a[i] = 0;
+    for (i = 0; i < n; ++i) 
+        printf("%d ", a[i]);
+    return 0;
+}
---
+++ Задание 18/Задание 18 (5).cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int i, n, x, max = 0, min = 0, a[1000];
+    printf("Введите кол-во элементов N: ");
+    scanf_s("%d", &n);
+    printf("Введите элементы массива A: ");
+    for (i = 0; i < n; i++)
+        scanf_s("%d", &a[i]);
+    for (i = 0; i < n - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            x = a[i + 1];
+            a[i + 1] = a[i];
+            a[i] = x;
+        }
+    }
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
